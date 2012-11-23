7bf652c4d83b600fc5c7e69a29fab49df314c7b9
Roelf-Jan de Vries <roelfjan@q42.nl>
2012-11-23 20:38:18 +0100

    afbeeldingen toegevoegd

--- app/javascripts/script.js
+++ app/javascripts/script.js
@@ -10,13 +10,45 @@ var inlineScroll = (function(){
 		
 		setScreenDimensions();
 		setInlineScroll();
-
+		
+		parseRecipe();
 	}
 	
 	$(document).ready(function(){
 		init();	
 	});
 	
+	
+	function parseRecipe() {
+		console.log("parse recipe");
+		
+		
+		$.getJSON("http://localhost/recipepirate/bookmarklet/beautiful_json.js", function(data){
+			console.log(data);
+			//var forum = json.forum;
+			
+			for (var i = 0; i < data.length; i++) {
+			    var object = data[i];
+			    for (property in object) {
+			        var value = object[property];
+			        
+			        if(property == "ingredients"){
+			        	$('[data-role~="'+property+'"]').html(value);
+			        }
+			        
+			         if(property == "recipeInstructions"){
+			        	$container = $('[data-role~="nav-steps"]');
+			        	for (i=0; i <property.length; i++) {
+			        		var value = object[property][i];
+			        		console.log(value);
+			        	};
+			        }
+			        
+			        console.log(property + "=" + value); 
+			    }
+			}
+		});
+	}
 	
 	function setScreenDimensions() {		
 		var windowHeight = $(window).height();
