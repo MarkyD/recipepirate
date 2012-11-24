7c9da417634f7970d9e88b0fe6a701b486221ea1
Roelf-Jan de Vries <roelfjan@q42.nl>
2012-11-24 03:26:05 +0100

    stapnavigatie enzo

--- app/javascripts/script.js
+++ app/javascripts/script.js
@@ -2,11 +2,13 @@ var inlineScroll = (function(){
 	var $sidebar,
 		$content,
 		$inlineAnchors,
-		$tabLinks;
+		$tabLinks,
+		$tab;
 	
 	function init(){
 		$sidebar = $('[data-role~="section-sidebar"]');
-		$content = $('[data-role~="section-content"]');		
+		$content = $('[data-role~="section-content"]');
+		$tab = $('[data-role~="tab"]');		
 		
 		setScreenDimensions();
 		setTabNavigation();
@@ -60,24 +62,81 @@ var inlineScroll = (function(){
 			        	$navContainer = $('[data-role~="nav-steps"]');
 			        	$contentContainer = $('[data-role~="recipe-steps"]');
 			        	
-			        	console.log($contentContainer);
-			        	
 			        	for (i=0; i < object[property].length; i++) {
 			        		var value = object[property][i];
 			        		$navContainer.append("<li><a href='#step"+(i+1)+"'><span class='number'>"+(i+1)+"</span>"+value.value+"</a></li");
-			        		$contentContainer.append("<div id='step"+(i+1)+"' class='instruction' data-role='inline-anchor'>"+value.value+"</div>");			        					        		
+			        		$contentContainer.append("<div id='step"+(i+1)+"' class='instruction' data-role='inline-anchor'><span>"+(i+1)+"</span><p>"+value.value+"</p></div>");
+													        					        					        		
 			        	};
 			        }			        			        
 			    }
 			}
 		}).complete(function(){
-			
+			$('#step1').addClass("active");
+			$content.append("<a href='#step2' data-role='next-step' class='button-action button-down'>Arr! Next step!</a>")
 			// set inline scroll
 			$inlineAnchors = $('[data-role~="inline-anchor"]');			
 			setInlineScroll();	
+			
+			setNextStep();
 		});
+				
+	}
+	
+	function setNextStep() {
+		$button = $('[data-role~="next-step"]');
 		
-		
+		$button.live("click", function(){
+			// on click on a inline anchor
+							
+			var index = $(this).attr("href").match(/\d+/);			
+      
+      $content.animate( {scrollTop: topsArray[index-1]}, { 'easing': 'swing', duration: 500 } );
+		  		  
+		  if(index[0] <= topsArray.length){
+		  	
+		  	$thisParent = $("#step"+parseInt(index[0]-1));		  	
+		  	
+		  	// change background of parent		  		  	
+		  	$thisParent.animate({'background-color':'#FFF','color':'#D2D2D2'},500);
+		  	$thisParent.delay(500).queue(function() {
+		  		$(this).find("span").addClass("done");	
+		  	});
+		  	
+		  	var $smallNav = $('[data-role~="nav-steps"]').find('[href=#step'+parseInt(index[0]-1)+']');
+		  	$smallNav.animate({'color':'#D2D2D2'});
+		  	$smallNav.delay(500).queue(function() {
+		  		$(this).find("span").addClass("done");	
+		  	});
+		  			  	
+		  	// change background of next
+		  	$thisParent.next().animate({'background-color':'#FFFFCC'},500);
+		  	
+		  	var nextHeight = $thisParent.next().innerHeight();
+		  	$(this).animate( {scrollTop: topsArray[index-1]+nextHeight}, { 'easing': 'swing', duration: 500 } );
+		  	
+		  	// set correct link in next-button
+		  	$(this).attr("href","#step"+(parseInt(index[0])+1));
+		  			  	
+		  } else {
+		  	$button.fadeOut();
+		  	
+		  	$thisParent = $("#step"+parseInt(index[0]-1));		  	
+		  	
+		  	// change background of parent		  		  	
+		  	$thisParent.animate({'background-color':'#FFF','color':'#D2D2D2'},500);
+		  	$thisParent.delay(500).queue(function() {
+		  		$(this).find("span").addClass("done");	
+		  	});
+		  	
+		  	var $smallNav = $('[data-role~="nav-steps"]').find('[href=#step'+parseInt(index[0]-1)+']');
+		  	$smallNav.animate({'color':'#D2D2D2'});
+		  	$smallNav.delay(500).queue(function() {
+		  		$(this).find("span").addClass("done");	
+		  	});
+		  }
+		   
+		});
 	}
 	
 	function setTabNavigation(){
@@ -111,8 +170,9 @@ var inlineScroll = (function(){
 		var windowHeight = $(window).height();
 		var windowWidth = $(window).width();
 								
-		$sidebar.height(windowHeight - 80);
+		$sidebar.height(windowHeight - 100);
 		$content.height(windowHeight - 80);
+		$tab.height(windowHeight - 160);
 		
 		$content.width(windowWidth - 281);
 	}
