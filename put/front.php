543e7cc8500393dcdbb2c44c574906bb5571613d
DrDipper <markdibbets@gmail.com>
2012-11-24 13:59:40 +0100

    $#%^&*()_+

--- put/front.php
+++ put/front.php
@@ -1,4 +1,5 @@
 <?php
+DEFINE("MAGIC_QUOTES_GPC", ini_get("magic_quotes_gpc")); 
 
 function append($file, $data)
 {
@@ -18,18 +19,33 @@ function append($file, $data)
 
 $userName  = $_GET['username'];
 //echo $userName;
-if ($_POST['recipe-pirate-output']){
-  //$jsonOutput = stripslashes($_POST['recipe-pirate-output']);
-  $jsonOutput = ($_POST['recipe-pirate-output']);
+if ($_POST['output']){
+  if(MAGIC_QUOTES_GPC) { 
+    //echo 'quotes';//'$name = $_POST['data']; 
+    $jsonOutput = stripslashes($_POST['output']);
+  } 
+  else { 
+    //$name = addslashes($_POST['data']); 
+    //echo 'no quotes';
+    $jsonOutput = ($_POST['output']);
+  }
   append('../recipes/'.$userName.'.js', $jsonOutput);
   }
-  /*
-echo $_POST['recipe-pirate-output']; 
-echo '****';
-echo $jsonOutput.'<br />';  
-echo '****';
-*/
-echo @file_get_contents('../recipes/'.$userName.'.js');
+  
+//echo @file_get_contents('../recipes/'.$userName.'.js');
 //echo stripslashes($str);
 ?>
-
+<div class="container">				
+    <h1>Yarr!!!</h1>
+    <p>
+      The recipe <strong id="result-user-title">[title]</strong>
+      has been robbed and saved!
+    </p>
+    <p>
+      You can find the kitchenfriendly view
+      at <a id="result-user-link" href="#">www.recipepirate.com/pocaripat</a>
+    </p>
+</div>
+<div class="recipe-actions">
+  <input type="button" id="button-whatever" class="button-action" value="Okay, whatever" />
+</div>
